61570e77b70f336735f7ca25e4f70365841c2dcc
Micha <micha-elmans@outlook.com>
2024-03-15 09:30:29 +0100

    setup the player

---
+++ BlackJack/Player.cs
@@ -0,0 +1,48 @@
+﻿namespace BlackJack;
+
+public class Player
+{
+
+    public int Score { get; set; }
+    public string Name { get; set; }
+
+    public Hand PlayerHand { get; set; } = new Hand();
+
+    public void DisplayHand()
+    {
+        Console.WriteLine($"Player: {Name}");
+        PlayerHand.DisplayHand();
+        Console.WriteLine($"Hand Value: {PlayerHand.GetHandValue()}");
+    }
+
+    public void AddCardToHand(Card card)
+    {
+        PlayerHand.AddCard(card);
+    }
+
+    public bool IsBusted()
+    {
+        return PlayerHand.GetHandValue() > 21;
+    }
+
+    public void Hit()
+    {
+        //
+    }
+
+    public void Stand()
+    {
+        //
+    }
+
+    public void DoubleDown()
+    {
+        //
+    }
+
+    public void Split()
+    {
+        //
+    }
+
+}

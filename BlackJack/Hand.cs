21782e6978e2b392deda207d60e5addfe7b7eacd
Micha <micha-elmans@outlook.com>
2024-03-15 09:15:46 +0100

    setup the hand class

---
+++ BlackJack/Hand.cs
@@ -0,0 +1,31 @@
+﻿namespace BlackJack;
+
+public class Hand
+{
+
+public List<Card> Cards { get; set; } = new List<Card>();
+
+    public void AddCard(Card card)
+    {
+        Cards.Add(card);
+    }
+
+    public int GetHandValue()
+    {
+        int handValue = 0;
+        foreach (var card in Cards)
+        {
+            handValue += card.GetValue();
+        }
+        return handValue;
+    }
+
+    public void DisplayHand()
+    {
+        foreach (var card in Cards)
+        {
+            Console.WriteLine(card.GetCardName());
+        }
+    }
+
+}

819b9c5f789da7be762d1bd18961413beabfc278
Micha <micha-elmans@outlook.com>
2024-03-18 13:44:36 +0100

    added a star of the Deck

---
+++ BlackJack/Deck.cs
@@ -0,0 +1,55 @@
+﻿namespace BlackJack;
+
+public class Deck
+{
+    public List<Card> Cards { get; set; }
+
+    public Deck()
+    {
+        Cards = new List<Card>();
+        foreach (Card.Suit suit in Enum.GetValues(typeof(Card.Suit)))
+        {
+            foreach (Card.Rank rank in Enum.GetValues(typeof(Card.Rank)))
+            {
+                Cards.Add(new Card(suit, rank));
+            }
+        }
+    }
+
+    public void Shuffle()
+    {
+        Random rng = new Random();
+
+        foreach (var card in Cards)
+        {
+            int number = Cards.IndexOf(card);
+            int randomIndex = rng.Next(number + 1);
+            (Cards[randomIndex], Cards[number]) = (Cards[number], Cards[randomIndex]);
+
+        }
+    }
+
+    public Card? DrawCard()
+    {
+        if (IsEmpty())
+        {
+            Card card = Cards[0];
+            Cards.RemoveAt(0);
+            return card;
+        }
+        return null;
+    }
+
+    public void PrintDeck()
+    {
+        foreach (Card card in Cards)
+        {
+            Console.WriteLine(card.GetCardName());
+        }
+    }
+
+    public bool IsEmpty()
+    {
+        return Cards.Count == 0;
+    }
+}
